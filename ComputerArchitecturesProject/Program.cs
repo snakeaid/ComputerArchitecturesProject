675bbe990940e52473ef2580162b2d25e92eb26c
illia <illia.antypenko@gmail.com>
2024-03-15 12:38:21 +0200

    Add stdin functionality

--- ComputerArchitecturesProject/Program.cs
+++ ComputerArchitecturesProject/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿var lines = ReadLines();
+
+string[] ReadLines()
+{
+    var lines = new List<string>();
+    
+    while (true)
+    {
+        var line = Console.ReadLine();
+        if(line == string.Empty)
+            break;
+        
+        lines.Add(line);
+    }
+
+    return lines.ToArray();
+}
