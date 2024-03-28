06a2d50deb4da40c4c3d304dbe2e6d4904408d2a
illia <illia.antypenko@gmail.com>
2024-03-28 21:05:03 +0200

    Add printing logic

--- Lab.asm
+++ Lab.asm
@@ -206,10 +206,138 @@ loop_calc_average:
     ; Next average calc
     inc bx                          ; Next key
     jmp loop_calc_average           ; Continue loop
-    
+
 ; Loop calc average end
 loop_calc_average_end:
     ; Sort
     call bubble_sort                ; Sort array
     ; Print keys
     xor cx, cx                      ; CX <-- 0
+
+loop_print_result:
+    ; Check keys
+    cmp cx, countKeys               ; Has more keys ?
+    jge loop_print_result_exit      ; Done
+    ; Print result
+    lea di, arrayKeys               ; DI <-- Key from array
+    mov bx, cx
+    imul bx, KEY_SIZE
+    lea dx, byte ptr ds:[di + bx]
+    call line_print                 ; Print current key
+    ; Next average calc
+    inc cx                          ; Next key
+    jmp loop_print_result           ; Continue loop
+; Loop print result exit
+loop_print_result_exit:
+    ; Exit program
+    mov al, 00h                     ; Exit code
+    mov ax, 4C00h                   ; DOS exit program with AL = exit code
+    int 21h                         ; DOS interrupt
+    ret                             ; Exit function
+
+
+; New line check
+is_new_line proc
+    ; Check EOL
+    cmp al, EOL                     ; EOL ?
+    je is_new_line_true             ; Done
+    ; Check LF
+    cmp al, LF                      ; LF ?
+    je is_new_line_true             ; Done
+    ; Check CR
+    cmp al, CR                      ; CR ?
+    je is_new_line_true             ; Done
+    ; Not a new line
+    mov ah, FALSE
+    ret                             ; Exit function
+; New line symbol
+is_new_line_true:
+    ; New line
+    mov ah, TRUE
+    ret                             ; Exit function
+is_new_line endp
+
+
+; Space check
+is_space proc
+    ; Check HTAB
+    cmp al, HTAB                    ; HTAB ?
+    je is_space_true                ; Done
+    ; Check VTAB
+    cmp al, VTAB                    ; VTAB ?
+    je is_space_true                ; Done
+    ; Check SPACE
+    cmp al, SPACE                   ; SPACE ?
+    je is_space_true                ; Done
+    ; Not a space
+    mov ah, FALSE
+    ret                             ; Exit function
+; Space symbol
+is_space_true:
+    ; Space
+    mov ah, TRUE
+    ret                             ; Exit function
+is_space endp
+
+
+; Read line function
+line_read proc
+    cld                             ; Clear dir flag
+    lea dx, dataLine                ; Line receive buffer
+    mov si, dx                      ; Buffer address to SI
+    xor bx, bx                      ; BX <-- 0
+    
+; Read single char
+loop_read_char:
+    ; DOS Read char
+    mov ah, 01h                     ; DOS Read char
+    int 21h                         ; DOS interrupt
+    ; Check new line
+    call is_new_line
+    cmp ah, TRUE                    ; New line ?
+    je loop_read_char_exit          ; Done
+    ; Check EOF
+    cmp al, EOF                     ; EOF ?
+    je loop_read_char_exit          ; Done
+    ; Store char
+    mov byte ptr ds:[si + bx], al   ; Store char to temp storage
+    inc bx                          ; Next char
+    cmp bx, LINE_SIZE               ; Line filled ?
+    jge loop_read_char_exit         ; Done
+    jmp loop_read_char              ; Continue loop
+
+; Read single char exit
+loop_read_char_exit:
+    ; Store EOL
+    mov byte ptr ds:[si + bx], EOL  ; Store EOL to temp storage
+    ret                             ; Exit function
+line_read endp
+
+; Print line function
+line_print proc
+    cld                             ; Clear dir flag
+    mov di, dx                      ; Buffer address to DI
+    xor bx, bx                      ; BX <-- 0
+
+; Print single char
+loop_print_char:
+    mov dl, byte ptr ds:[di + bx]   ; Copy line char to DL
+    cmp dl, EOL                     ; Line end ?
+    je loop_print_char_exit         ; Done
+    mov ah, 02h                     ; DOS Print char
+    int 21h                         ; DOS interrupt
+    inc bx                          ; Next char
+    jmp loop_print_char             ; Continue loop
+
+; Print single char exit
+loop_print_char_exit:
+    ; Print CR
+    mov dl, CR                      ; Copy CR char to DL
+    mov ah, 02h                     ; DOS Print char
+    int 21h                         ; DOS interrupt
+    ; Print LF
+    mov dl, LF                      ; Copy LF char to DL
+    mov ah, 02h                     ; DOS Print char
+    int 21h                         ; DOS interrupt
+    ret                             ; Exit function
+line_print endp
