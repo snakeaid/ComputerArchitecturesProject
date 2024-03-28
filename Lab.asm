9bd2eefdcea4407c116e7415c713318924fc0410
illia <illia.antypenko@gmail.com>
2024-03-28 18:17:09 +0200

    Add data segment

---
+++ Lab.asm
@@ -0,0 +1,75 @@
+; Memory model
+.MODEL SMALL
+; Stack size
+.STACK 100h
+
+
+; CPU Arch
+.386
+
+
+; Data segment
+.DATA
+
+    ; FALSE
+    FALSE equ 00h
+    ; TRUE
+    TRUE equ 0FFh
+
+    ; EOL
+    EOL equ 00h
+    ; HOR. TAB
+    HTAB equ 09h
+    ; LF
+    LF equ 0Ah
+    ; VERT. TAB
+    VTAB equ 0Bh
+    ; CR
+    CR equ 0Dh
+    ; SPACE
+    SPACE equ 20h
+    ; EOF
+    EOF equ 1Ah
+
+    ; Maximum value
+    MAX_VALUE equ 10000
+    ; Minimum value
+    MIN_VALUE equ -10000
+
+    ; Maximum lines count
+    MAX_LINE_COUNT equ 10000
+
+    ; Line buffer size
+    LINE_SIZE equ 32
+    ; Key buffer size
+    KEY_SIZE equ 16
+    ; Value buffer size
+    VALUE_SIZE equ 8
+
+    ; Buffer to receive line
+    dataLine db LINE_SIZE DUP(EOL),'$'
+
+    ; Buffer for parsed key
+    dataKey db KEY_SIZE DUP(EOL),'$'
+    ; Buffer for parsed value
+    dataValue db VALUE_SIZE DUP(EOL),'$'
+
+    ; Parsed value sign
+    dataValueSign db 0
+    ; Parsed binary value
+    dataValueBin dw 0
+
+    ; Received lines count
+    countLines dw 0
+
+    ; Received keys count
+    countKeys dw 0
+
+    ; Keys
+    arrayKeys db LINE_SIZE*KEY_SIZE DUP(EOL),'$'
+    ; Sum of key values
+    arraySum dw  LINE_SIZE DUP(0)
+    ; Count values
+    arrayCount dw LINE_SIZE DUP(0)
+    ; Average values
+    arrayAverage dw LINE_SIZE DUP(0)
